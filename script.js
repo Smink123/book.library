da8636d8154d91041a9662bde47d129842c5cb46
Sarah Harvey <sarahcharvey@hotmail.com>
2023-10-12 11:35:51 +0100

    added basic css styling, and implimented read/not read buttons

--- script.js
+++ script.js
@@ -8,12 +8,13 @@ const totalBooks = document.getElementById('totalBooks')
 
 const myLibrary = [];
 
-function Book(bookTitle, author, totalPages, yearPublished, haveCompleted) {
-  this.bookTitle = bookTitle;
-  this.author = author;
-  this.totalPages = totalPages;
-  this.yearPublished = yearPublished;
-}
+function Book(bookTitle, author, totalPages, yearPublished, notRead) {
+    this.bookTitle = bookTitle;
+    this.author = author;
+    this.totalPages = totalPages;
+    this.yearPublished = yearPublished;
+    this.notRead = notRead || false;
+  }
 
 submitBook.disabled = true;
 
@@ -34,41 +35,85 @@ numberOfPages.addEventListener("input", validForms);
 publishYear.addEventListener("input", validForms);
 
 function displayBooks() {
-  while (bookLogs.firstChild) {
-    bookLogs.removeChild(bookLogs.firstChild);
-  }
-  myLibrary.forEach((book, i) => {
-    const userInfoBox = document.createElement("div");
-    userInfoBox.classList.add("bookDisplay");
-    bookLogs.appendChild(userInfoBox);
-
-    const userTitleAndAuthor = document.createElement("p");
-    userTitleAndAuthor.textContent = `${book.bookTitle} by ${book.author}`;
-    userInfoBox.appendChild(userTitleAndAuthor);
-
-    const userPagesAndYear = document.createElement("p");
-    userPagesAndYear.textContent = `${book.totalPages} pages, ${book.yearPublished}`;
-    userInfoBox.appendChild(userPagesAndYear);
-
-    const removeButton = document.createElement("button");
-    removeButton.textContent = 'REMOVE';
-    removeButton.classList.add('remove');
-    userInfoBox.appendChild(removeButton);
-
-    removeButton.addEventListener('click', function() {
+    while (bookLogs.firstChild) {
+      bookLogs.removeChild(bookLogs.firstChild);
+    }
+    myLibrary.forEach((book, i) => {
+      const userInfoBox = document.createElement("div");
+      userInfoBox.classList.add("bookDisplay");
+      bookLogs.appendChild(userInfoBox);
+  
+      const userTitleAndAuthor = document.createElement("p");
+      userTitleAndAuthor.textContent = `${book.bookTitle} by ${book.author}`;
+      userInfoBox.appendChild(userTitleAndAuthor);
+  
+      const userPagesAndYear = document.createElement("p");
+      userPagesAndYear.textContent = `${book.totalPages} pages, ${book.yearPublished}`;
+      userInfoBox.appendChild(userPagesAndYear);
+  
+      const removeButton = document.createElement("button");
+      removeButton.textContent = 'REMOVE';
+      removeButton.classList.add('remove');
+      userInfoBox.appendChild(removeButton);
+  
+      removeButton.addEventListener('click', function() {
         myLibrary.splice(i, 1);
         displayBooks();
+      });
+  
+      const statusBox = document.createElement("div");
+      statusBox.classList.add('statusBox');
+      userInfoBox.appendChild(statusBox);
+  
+      const haveRead = document.createElement("button");
+      haveRead.classList.add('statusButtons');
+      haveRead.textContent = 'NOT READ';
+      statusBox.appendChild(haveRead);
+  
+      const notRead = document.createElement("button");
+      notRead.classList.add('statusButtons');
+      notRead.textContent = 'HAVE READ';
+      statusBox.appendChild(notRead);
+  
+      if (book.notRead) { // the automatic state of the buttons/selection. haveRead starts off as 'true'
+        notRead.style.backgroundColor = 'black';
+        notRead.style.color = 'white';
+        haveRead.style.backgroundColor = 'white';
+        haveRead.style.color = 'black';
+      } else {
+        haveRead.style.backgroundColor = 'black';
+        haveRead.style.color = 'white';
+        notRead.style.backgroundColor = 'white';
+        notRead.style.color = 'black';
+      }
+  
+      haveRead.addEventListener('click', function() {
+        book.notRead = false; //change the state to it has been read
+        haveRead.style.backgroundColor = 'black';
+        haveRead.style.color = 'white';
+        notRead.style.backgroundColor = 'white';
+        notRead.style.color = 'black';
+      });
+  
+      notRead.addEventListener('click', function() {
+        book.notRead = true; //change the state to
+        notRead.style.backgroundColor = 'black';
+        notRead.style.color = 'white';
+        haveRead.style.backgroundColor = 'white';
+        haveRead.style.color = 'black';
+      });
     });
-  });
-  totalBooks.textContent = `Total Books: ${myLibrary.length}`;
-}
+    totalBooks.textContent = `Total Books: ${myLibrary.length}`;
+  }
+  
 
 submitBook.addEventListener("click", function () {
   const title = titleOfBook.value;
   const author = authorOfBook.value;
   const pages = numberOfPages.value;
   const year = publishYear.value;
-  let bookAdded = new Book(title, author, pages, year);
+  let haveRead = false;
+  let bookAdded = new Book(title, author, pages, year, haveRead);
   myLibrary.push(bookAdded);
 
   displayBooks();
@@ -79,58 +124,3 @@ submitBook.addEventListener("click", function () {
   publishYear.value = "";
   submitBook.disabled = true;
 });
-
-
-
-
-
-
-
-
-/*
-  while (bookLogs.firstChild) {
-    bookLogs.removeChild(bookLogs.firstChild);
-  }
-  for (let i = 0; i < myLibrary.length; i++) {
-    const userInfoBox = document.createElement("div");
-    userInfoBox.classList.add("bookDisplay");
-    bookLogs.appendChild(userInfoBox);
-
-    const userTitleAndAuthor = document.createElement("p");
-    userTitleAndAuthor.textContent = `${myLibrary[i].bookTitle} by ${myLibrary[i].author}`;
-    userInfoBox.appendChild(userTitleAndAuthor);
-
-    const userPagesAndYear = document.createElement("p");
-    userPagesAndYear.textContent = `${myLibrary[i].totalPages} pages, ${myLibrary[i].yearPublished}`;
-    userInfoBox.appendChild(userPagesAndYear);
-
-    const removeButton = document.createElement("button")
-    removeButton.textContent = 'REMOVE'
-    removeButton.classList.add('remove')
-    userInfoBox.appendChild(removeButton);
-  }
-});
-
-for (let i = 0; i < myLibrary.length; i++) {
-    removeButton.addEventListener('click', function() {
-        myLibrary.splice(myLibrary[i], 1)
-        bookLogs.textContent = ''
-
-        const userInfoBox = document.createElement("div");
-    userInfoBox.classList.add("bookDisplay");
-    bookLogs.appendChild(userInfoBox);
-
-    const userTitleAndAuthor = document.createElement("p");
-    userTitleAndAuthor.textContent = `${myLibrary[i].bookTitle} by ${myLibrary[i].author}`;
-    userInfoBox.appendChild(userTitleAndAuthor);
-
-    const userPagesAndYear = document.createElement("p");
-    userPagesAndYear.textContent = `${myLibrary[i].totalPages} pages, ${myLibrary[i].yearPublished}`;
-    userInfoBox.appendChild(userPagesAndYear);
-
-    const removeButton = document.createElement("button")
-    removeButton.textContent = 'REMOVE'
-    removeButton.classList.add('remove')
-    userInfoBox.appendChild(removeButton);
-    })
-}*/
